2e01e8a0e22b949a7547ce6f5a1e8275fa5682fa
Matthew Kling <mattkling@gmail.com>
2015-12-21 23:21:45 -0800

    colors2d interpolated from 4 corner colors

--- R/colomap.R
+++ R/colomap.R
@@ -1,7 +1,7 @@
 
 
 
-#' Get colors for a 3D dataset.
+#' Get colors from RGB space for a 3D dataset.
 #'
 #' This function returns a color value for each row of the 3-column dataset
 #' supplied, by transforming the input data and using it as RGB values.
@@ -28,3 +28,28 @@ colors3d <- function(data, trans="fit", order=1, inversion=1, opacity=NULL){
       cols[which(is.finite(rowMeans(data)))] <- rgb(na.omit(data))
       cols
 }
+
+
+#' Get colors from a 4-color plane for a 2D dataset.
+#'
+#' This function returns a color value for each row of the 2-column dataset
+#' supplied, based on a 2D color palette interpolated from 4 corner colors.
+#'
+#' @param data Matrix or data frame with 3 numeric columns.
+#' @param colors Vector of 4 corner colors to interpolate, clockwise from upper
+#'   right.
+#' @return Vector of color values.
+
+colors2d <- function(data, colors=c("yellow", "green", "blue", "magenta")){
+      colors <- col2rgb(colors)/255
+      data <- apply(data, 2, scales::rescale)
+      interpolate <- function(i){
+            x <- i[1]
+            y <- i[2]
+            x1 <- colors[,2] * x + colors[,3] * (1-x)
+            x2 <- colors[,1] * x + colors[,4] * (1-x)
+            x2 * y + x1 * (1-y)
+      }
+      rgb(t(apply(data, 1, interpolate)))
+}
+
